ece94a96c67d01740d8efb6505874daf71f5f67c
Ari Seyhun <ariseyhun@live.com.au>
2022-03-23 20:33:10 +1030

    test: add schema parse tests

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -18,3 +18,178 @@ use self::schema::Schema;
 pub fn parse<'i>(input: impl Into<Span<'i>>) -> Result<Schema<'i>, Error<Span<'i>>> {
     final_parser(parse_schema)(input.into())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        aggregate::{Aggregate, Command, Param, ReturnType, ReturnTypeOptionalOrRequired},
+        event::{Event, Field},
+        parse,
+        schema::Schema,
+        types::{OptionalOrRequiredType, Scalar, ScalarOrUserType, Type},
+        Error, Span,
+    };
+
+    #[test]
+    fn it_parses_basic_schema() -> Result<(), Error<Span<'static>>> {
+        let schema_str = r#"
+            aggregate Hello {
+                world(name: String) -> FooEvent
+            }
+
+            event FooEvent {
+                name: String
+            }
+        "#;
+
+        let expected = Schema {
+            aggregates: vec![Aggregate {
+                ident: "Hello",
+                commands: vec![Command {
+                    ident: "world",
+                    params: vec![Param {
+                        ident: "name",
+                        ty: Type::Single(OptionalOrRequiredType::Required(
+                            ScalarOrUserType::Scalar(Scalar::String),
+                        )),
+                    }],
+                    return_type: ReturnType::Single(ReturnTypeOptionalOrRequired::Required(
+                        "FooEvent",
+                    )),
+                }],
+            }],
+            events: vec![Event {
+                ident: "FooEvent",
+                fields: vec![Field {
+                    ident: "name",
+                    ty: Type::Single(OptionalOrRequiredType::Required(ScalarOrUserType::Scalar(
+                        Scalar::String,
+                    ))),
+                }],
+            }],
+            types: vec![],
+        };
+
+        assert_eq!(parse(schema_str)?, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_parses_bank_account_schema() -> Result<(), Error<Span<'static>>> {
+        let schema_str = r#"
+          aggregate BankAccount {
+            open_account(name: String?, initial_balance: Float) -> OpenedAccount?
+            deposit_funds(amount: Float) -> DepositedFunds?
+            withdraw_funds(amount: Float) -> WithdrewFunds?
+          }
+          
+          event OpenedAccount {
+            name: String?
+            initial_balance: Float
+          }
+          
+          event DepositedFunds {
+            amount: Float
+          }
+          
+          event WithdrewFunds {
+            amount: Float
+          }          
+        "#;
+
+        let expected = Schema {
+            aggregates: vec![Aggregate {
+                ident: "BankAccount",
+                commands: vec![
+                    Command {
+                        ident: "open_account",
+                        params: vec![
+                            Param {
+                                ident: "name",
+                                ty: Type::Single(OptionalOrRequiredType::Optional(
+                                    ScalarOrUserType::Scalar(Scalar::String),
+                                )),
+                            },
+                            Param {
+                                ident: "initial_balance",
+                                ty: Type::Single(OptionalOrRequiredType::Required(
+                                    ScalarOrUserType::Scalar(Scalar::Float),
+                                )),
+                            },
+                        ],
+                        return_type: ReturnType::Single(ReturnTypeOptionalOrRequired::Optional(
+                            "OpenedAccount",
+                        )),
+                    },
+                    Command {
+                        ident: "deposit_funds",
+                        params: vec![Param {
+                            ident: "amount",
+                            ty: Type::Single(OptionalOrRequiredType::Required(
+                                ScalarOrUserType::Scalar(Scalar::Float),
+                            )),
+                        }],
+                        return_type: ReturnType::Single(ReturnTypeOptionalOrRequired::Optional(
+                            "DepositedFunds",
+                        )),
+                    },
+                    Command {
+                        ident: "withdraw_funds",
+                        params: vec![Param {
+                            ident: "amount",
+                            ty: Type::Single(OptionalOrRequiredType::Required(
+                                ScalarOrUserType::Scalar(Scalar::Float),
+                            )),
+                        }],
+                        return_type: ReturnType::Single(ReturnTypeOptionalOrRequired::Optional(
+                            "WithdrewFunds",
+                        )),
+                    },
+                ],
+            }],
+            events: vec![
+                Event {
+                    ident: "OpenedAccount",
+                    fields: vec![
+                        Field {
+                            ident: "name",
+                            ty: Type::Single(OptionalOrRequiredType::Optional(
+                                ScalarOrUserType::Scalar(Scalar::String),
+                            )),
+                        },
+                        Field {
+                            ident: "initial_balance",
+                            ty: Type::Single(OptionalOrRequiredType::Required(
+                                ScalarOrUserType::Scalar(Scalar::Float),
+                            )),
+                        },
+                    ],
+                },
+                Event {
+                    ident: "DepositedFunds",
+                    fields: vec![Field {
+                        ident: "amount",
+                        ty: Type::Single(OptionalOrRequiredType::Required(
+                            ScalarOrUserType::Scalar(Scalar::Float),
+                        )),
+                    }],
+                },
+                Event {
+                    ident: "WithdrewFunds",
+                    fields: vec![Field {
+                        ident: "amount",
+                        ty: Type::Single(OptionalOrRequiredType::Required(
+                            ScalarOrUserType::Scalar(Scalar::Float),
+                        )),
+                    }],
+                },
+            ],
+            types: vec![],
+        };
+
+        assert_eq!(parse(schema_str)?, expected);
+
+        Ok(())
+    }
+}
